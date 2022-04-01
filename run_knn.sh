dfcbc7d6b2aa2035c40c1e7cfb7bedc4218a30e4
JuanCervino <JuanCervino>
2022-04-01 16:00:41 -0400

    Adding functionalities

--- run_knn.sh
+++ run_knn.sh
@@ -1,13 +1,23 @@
 # python -m smooth.scripts.compute_knn --dataset CIFAR10 
 python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model resnet18 --layer_n 1
-# python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model resnet18 --layer_n 2
-# python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model resnet18 --layer_n 3
-# python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model resnet18 --layer_n 4
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model resnet18 --layer_n 2
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model resnet18 --layer_n 3
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model resnet18 --layer_n 4
 
-# python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 1
-# python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 2
-# python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 3
-# python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 4
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 1
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 2
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 3
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 4
+
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric euclidean --pretrained imagenet --model resnet18 --layer_n 1
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric euclidean --pretrained imagenet --model resnet18 --layer_n 2
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric euclidean --pretrained imagenet --model resnet18 --layer_n 3
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric euclidean --pretrained imagenet --model resnet18 --layer_n 4
+
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 1
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 2
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 3
+python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms None --metric cosine_similarity --pretrained imagenet --model resnet18 --layer_n 4
 
 # python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model alexnet --layer_n 1
 # python -m smooth.scripts.compute_knn --dataset CIFAR10  --transforms normalized --metric euclidean --pretrained imagenet --model alexnet --layer_n 2
